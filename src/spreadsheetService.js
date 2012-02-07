94e9fcb8b2427bccf86ead229a746e3ac5342f2f
soundTricker <keisuke.oohashi@gmail.com>
2012-02-07 11:24:03 +0900

    Bug Fix.

--- src/spreadsheetService.js
+++ src/spreadsheetService.js
@@ -1,11 +1,113 @@
 /**
  * SpreadsheetService (Spreadsheet As A DB)
+ * @version : 1.0.1
+ * @author keisuke (soundTricker318)
  * This Script can be Spreadsheet as a DB for Google Apps Script.
  * And it can do select/insert/update/delete Row(s) on Spreadsheet  by query.
  * it use List Base Feed on Google Spreadsheet API.
  * if you need query reference, you see Google Spreadsheet API reference.
- * And maybe it can full text query to Spreadsheet rows.
+ * And it can full text query to Spreadsheet rows.
+ * Caution :Maybe List Base Feed is not support query containts "-" character.if you need contains "-" char,Use full text query.
  */
+/*
+
+/** 
+ *  Release Note
+ * 2012/02/05 1.0.0 first release 
+ * 2012/02/07 1.0.1 bug fix and chage logic. if fetch error,not catch error.
+ */
+
+/*
+****Example Code*****
+Ready Spreadsheet.
+sheet as table on Spreadsheet.
+Row1 is Header.
+*******************************
+function testCode() {
+  var ss = SpreadsheetApp.getActive();
+  var spreadsheetService = new SpreadsheetService(ss.getId());
+  //if you need set ConsumerKey and ConsumerSecret,new SpreadsheetService(ss.getId() , "consumerKey" , "consumerSecret");
+
+  //When you use SpreadsheetService,You should call init method.
+  spreadsheetService.init(); //When your first call,Need oauth authentification.
+  
+  var entry = {
+    "id":1,
+    "name":"soundTricker"
+  };
+  
+  var entry2 = {
+    "id":2,
+    "name": "soundTricker2"
+  };
+  
+  var entry3 = {
+    "id":3,
+    "name": "soundTricker2"
+  };
+  
+  //insert
+  //If your need insert row, you user insert method
+  //insert method args
+  //arg0 : sheetName
+  //arg1 : insert object, {column : value}
+  spreadsheetService.insert(ss.getSheets()[0].getName(), entry);
+
+  //insert
+  spreadsheetService.insert(ss.getSheets()[0].getName(), entry2);
+  spreadsheetService.insert(ss.getSheets()[0].getName(), entry3);
+
+  
+  //select
+  //If you need search rows,you use query method.
+  //query method args
+  //arg0 : sheetName
+  //arg1 : query. query reference is http://code.google.com/intl/ja/apis/spreadsheets/data/3.0/reference.html#ListParameters
+  //arg2 : advanceObject
+  //if you need sorted result or revesed result or full-text query,you set advanceObject.
+  //advanceObject field is 
+  // { orderby : "column:columnName" ,//Specifies what column to use in ordering the entries in the feed.
+  //   reverse:true/false , // Specifies whether to sort in descending or ascending order.
+  //   q: full-text-query for rows
+  // };
+  
+  //if you need all rows,arg1 set empty string.
+  var rows = spreadsheetService.query(ss.getSheets()[0].getName(), "");  //maybe rows length is 3, and rows[0]'s is 1, rows[1]'s id is 2, rows[2]'s id is 3
+
+  //order desc and filter and 
+  rows = spreadsheetService.query(ss.getSheets()[0].getName() , "name=soundTricker2" ,{orderby : "column:id" , reverse : true});
+  
+  //query result always return as a array. if result length is 1.
+  var row = spreadsheetService.query(ss.getSheets()[0].getName() , "name=soundTricker2 && id=3");
+  
+  row[0].name = "soundTricker3";
+  
+  //update
+  //If you need update row,you use update method.
+  //update method args
+  //arg0 : sheetName
+  //arg1 : update object. it shoud be selected object.
+  spreadsheetService.update(ss.getSheets()[0].getName() , row[0]);
+  
+  
+  //full text search
+  var row = spreadsheetService.query(ss.getSheets()[0].getName() , "" , {q:"soundTricker3"});
+  
+  
+  //delete
+  //If you need delete row,you use delete method
+  //delete method args
+  //arg0 ; sheetName
+  //arg1 : delete object. it shoud be selected object
+  spreadsheetService.deleteEntry(ss.getSheets()[0].getName() , row[0]);
+  
+  
+  //if you update sheetName or add sheet.
+  //you should call refleshListKey method
+  spreadsheetService.refleshListKey();
+}
+
+*/
 /*
  * The MIT License
  * 
@@ -198,7 +300,6 @@
       }
       var url = "https://spreadsheets.google.com/feeds/list/" + this.key + "/" + this.sheetList[sheetName] +  "/private/full?alt=json&prettyprint=false&v=3.0&sq=" + encodeURIComponent(queryString) + optionString;
       var res =  UrlFetchApp.fetch(url,this.oauthOptions);
-      
       var j = Utilities.jsonParse(res.getContentText());
       
       var entries = j.feed.entry;
